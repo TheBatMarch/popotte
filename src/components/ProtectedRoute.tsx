8f601f9f5648091610547fa88d987f24050bdb01
TheBatMarch <baptiste.marchand14@gmail.com>
2025-07-04 22:21:44 +0200

    Fix TypeScript unused parameter error

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -7,5 +7,9 @@ interface ProtectedRouteProps {
 
 export function ProtectedRoute({ children, adminOnly = false }: ProtectedRouteProps) {
   // Temporairement désactivé pour les tests
+  // Reference adminOnly to satisfy TypeScript compiler
+  if (adminOnly) {
+    // Future admin-only logic will go here
+  }
   return <>{children}</>
 }
